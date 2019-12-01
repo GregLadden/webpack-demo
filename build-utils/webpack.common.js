49ac68f4ba17141ae2462ddff0d6aa4ea3808cad
GregLadden <50276121+GregLadden@users.noreply.github.com>
2019-11-30 21:38:42 -0600

    adding css img and workbox

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -10,8 +11,25 @@ module.exports = {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      }
-    ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ],
+      },
+    ],
   },
   resolve: {
     extensions: ['*', '.js']
@@ -21,6 +39,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Webpack Demo',
       template: './src/index.html'
+    }),
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
     })
   ],
   output: {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import User from './testing'
+import './scss/style.scss'
+import heroImg from './assets/img/Hero.jpg'
 
 // const helloYou = hello()
 const greg = new User('Greg', 'Ladden')
@@ -6,3 +8,13 @@ const greg = new User('Greg', 'Ladden')
 document.getElementById('testing').addEventListener('click', () => {
   return alert(`${greg.firstName} ${greg.lastName} is ${greg.userAge(2019,1985)}`)
 })
+
+if ('serviceWorker' in navigator) {
+     window.addEventListener('load', () => {
+       navigator.serviceWorker.register('/service-worker.js').then(registration => {
+         console.log('SW registered: ', registration);
+       }).catch(registrationError => {
+         console.log('SW registration failed: ', registrationError);
+       });
+     });
+   }
